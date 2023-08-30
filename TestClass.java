67e866feb2bdd5dc18303a311c971b268a2ec030
Tea3822 <143539223+Tea3822@users.noreply.github.com>
2023-08-30 23:41:49 +0200

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,32 @@
+package integration;
+
+public class TestClass {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hellow World");
+        sayHellow("Sofija");
+        sayGoodbye("Tea");
+        sayGoodbye();
+
+        private int age =5;
+
+    }
+
+    public static void sayHellow(String name){
+        System.out.println("hellow" +name);
+
+    }
+    public static void sayGoodbye(String name){
+        System.out.println("Good Bye" +name);
+
+       // TODO: Add support
+
+        // tea i mile  komentar vo edna linija
+
+        /* dmvdk;v
+        vmdkvn
+        mvmdknv  kolku sakame vo poveke linii
+         */
+    }
+}
